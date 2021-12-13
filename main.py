234b7a363fb711fece958449889aba119afcbe9d
arifn1988 <41110831+arifn1988@users.noreply.github.com>
2021-12-13 10:54:19 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,227 +1,294 @@
 from datetime import date
-from datetime import datetime
+from datetime import timedelta
+import csv
 import argparse
+from collections import Counter
+from matplotlib import pyplot as plt
 import program
 
 file_bought= 'csv/bought.csv'
 file_sold='csv/sold.csv'
-settings='superpy/settings.txt'
-days = 0
 
 def main():
     args =get_arguments()
     start = args.pop('start')
-    
+
     if len(start)>2:
         print('Too many arguments for start')
         return False
-
-    if 'report' in start:
+    elif 'report' in start:
         if 'inventory' in start:
             report_inventory(args)
         elif 'profit' in start:
-            print(get_profit())
+            print(get_profit(args))
         elif 'revenue' in start:
-            print(get_revenue())
+            print(get_revenue(args))
         else :
             print('1:Select the right combination of command')
+    elif 'plot' in start: 
+        if'inventory' in start:
+            plot_inventory(args)
     elif 'buy' in start and len(start)==1:
         print(buy_product(args))
     elif 'sell' in start and len(start)==1:
         print(sell_product(args))
-    elif 'set' in start:
-        if -1>=args['time'] or args['time']>=0:
-            print('changing time')
-            change_days(args['time'])
-        else:
-            print('Set command must be paired with the command --time {#days}!')
+    elif 'set' in start and 'time' in args :
+        program.change_days(args['time'])
+    elif 'advance' in start and 'time' in args:
+        program.advance_time(args['time'])
+    elif 'clear' in start and len(start)==1:
+        clear_inventory()
     else:
         print('2:Select the right combination of commands')
 
 def sell_product(args):
-    product= get_product(file_bought,'product_name',args['product_name'])
+    product=get_product('product_name',args['product_name'])
+
     if product:
-        sale = [product_id(file_sold),product['id'],get_today(),args['price'],product['buy_price']]
-        product['status']='sold:'+str(get_today())
+        sale =[product_id(file_sold), product['id'],program.get_date(program.get_days()),product['buy_price'],args['price']]
         program.write_csv(file_sold,sale)
-        program.change_row(file_bought,product['id'],'inventory','sold/'+str(get_today()))
-        return product['product_name']+' sold.' 
+        program.change_row(file_bought,product['id'],'inventory','sold/'+str(program.get_date(program.get_days())))
+        return 'Product sold'
     else:
-        return 'This product is not in stock!!!'
+        return 'Product is not in inventory'
 
 def buy_product(args):
     if args['product_name'] and args['expiration_date'] and args['price']:
-        pname= args['product_name']
-        exp = program.format_to_iso(args['expiration_date'])
-        price = args['price']
-        product=[product_id(file_bought),pname,get_today(),price,exp,'inventory']
-        program.write_csv(file_bought,product)
-        return 'Product bought!!!'
+        exp_date= reverse_date(args['expiration_date'])
+        buy_date=program.get_date(program.get_days())
+        if program.format_time(exp_date)>= buy_date:
+            product=[product_id(file_bought),args['product_name'],buy_date,args['price'],exp_date,'inventory']
+            program.write_csv(file_bought,product)
+            return 'Product bought!!!'
+        else:
+            return 'Expiration date is greater than buy date'
     else:
-        return 'Can\'t by product'
+        return 'Add all the correct arguments to buy product'
 
-def get_profit():
-    csv= program.read_csv(file_sold)
-    header = next(csv)
-    buy_index= header.index('buy_price')
-    sell_index = header.index('sell_price')
-    sales = 0 
+def get_profit(args):
+    costs =0
+    csv = program.csv_to_dict(file_bought)
+    
+    if args['now'] or args['yesterday']:
+        time = get_time(args)
+        for line in csv:
+            buy_date = program.format_time(line['buy_date'])
+            exp_date = program.format_time(line['expiration_date'])
+            if 'inventory' in line['status']:
+                if buy_date==time:
+                    costs+=float(line['buy_price'])
+            elif 'sold' in line['status']:
+                sold_date = program.format_time(line['status'].split('/')[1])
+                if buy_date==time and sold_date<= time:
+                    costs+=float(line['buy_price'])
+    elif args['date']:
+        date = program.format_time(args['date'],'%Y-%m')
+        for line in csv:
+            buy_date = program.format_time(line['buy_date'])
+            if 'inventory' in line['status']:
+                exp_date = program.format_time(line['expiration_date'])
+                if buy_date.year == date.year and buy_date.month== date.month:
+                    costs+=float(line['buy_price'])
+            elif 'sold' in line['status']:
+                sold_date = program.format_time(line['status'].split('/')[1])
+                if buy_date.year == date.year and buy_date.month== date.month:
+                    costs+=float(line['buy_price'])
 
-    for line in csv:
-        sales+=int(line[sell_index])
+    return get_revenue(args)-costs
 
-    csv_inventory = program.read_csv(file_bought)
-    header= next(csv_inventory)
-    buy_index= header.index('buy_price')
-    cost= 0 
-
-    for line in csv_inventory:
-        cost+=int(line[buy_index])
-
-    return sales-cost
-
-def get_revenue():
+def get_revenue(args):
     revenue =0
-    csv = program.read_csv(file_sold)
-    header= next(csv)
-    p_index = header.index('sell_price')
-    
-    for line in csv:
-        revenue+=int(line[p_index])
-    
-    return revenue 
+    csv = program.csv_to_dict(file_sold)
+
+    if args['now'] or args['yesterday']:
+        time = get_time(args)
+        for line in csv:
+            sold_date =program.format_time(line['sell_date'])
+            if sold_date ==time:
+                revenue+=float(line['sell_price'])
+        return revenue
+    elif args['date']:
+        time= program.format_time(args['date'],'%Y-%m')
+        for line in csv:
+            sold_date= program.format_time(line['sell_date'])
+            if time.year == sold_date.year and time.month == sold_date.month:
+                revenue+=float(line['sell_price'])
+
+        return revenue
+
+    return 0
 
 """
     Gets the inventory from the buy-date and or expiration-date. If the parameters
     of the arguments are are null are passed as parameters it prints the entire inventory
 """
 def report_inventory(args):
-    product_list = program.csv_to_dict(file_bought)
+    products = program.csv_to_dict(file_bought)
     time =get_time(args)
-
-    if not time:
-        return 'No time passed through as args'
-
     inventory=[]
-    inventory.append(['Product','num','expiration date'])
 
-    while len(product_list)!=0:
-        product = product_list.pop(0)
-        buy_date=format_time(product['buy_date']) 
-        exp_date=format_time(product['expiration_date'])
+    while len(products)!=0:
+        product = products.pop(0)
+        buy_date=program.format_time(product['buy_date']) 
+        exp_date=program.format_time(product['expiration_date'])
         if product['status']=='inventory':
             if time>= buy_date and exp_date>=time:
-                inventory.append([product['id'],product['product_name'],product['buy_price'],product['expiration_date']])
+                item={'name':product['product_name'],'price':product['buy_price'],'exp':product['expiration_date']}
+                inventory.append(item)
         elif 'sold' in product['status']:
-            sold_date =format_time(product['status'].split('/')[1])
+            sold_date =program.format_time(product['status'].split('/')[1])
             if time >=buy_date and time<=sold_date:
-                inventory.append([product['id'],product['product_name'],product['buy_price'],product['expiration_date']])
+                item={'name':product['product_name'],'price':product['buy_price'],'exp':product['expiration_date']}
+                inventory.append(item)
+     
+    table=[]
 
     for item in inventory:
-        print(item)
+        if item not in table:
+            table.append(item)
+
+    for item in table:
+        count =0
+        for product in inventory:
+            if product== item:
+                count+=1
+        item['num']=count
+
+    header = ['product_name','price','num','expiration_date']
+    program.create_Table(header,table,'Inventory:'+str(program.get_date(program.get_days())))
 
     return inventory
 
+def plot_inventory(args):
+    csv_file= open(file_bought)
+    csv_reader = list(csv.DictReader(csv_file))
+
+    inventory={}
+    start_time = program.format_time(args['date'],'%m-%Y')
+    end_time =None
+    y_coordinates={}
+
+    if start_time.month == program.get_date(program.get_days()).month:
+        end_time= program.get_date(program.get_days())
+    else:
+        end_time=program.format_time('-'.join([str(start_time.month+1),str(start_time.year)]),'%m-%Y')
+
+    while start_time<=end_time:
+        products=[]
+
+        for product in csv_reader:
+            buy_date = program.format_time(product['buy_date'])
+            p_str= '/'.join([product['product_name'],product['buy_price'],product['expiration_date']])
+            if product['status'] =='inventory' :
+                exp_date= program.format_time(product['expiration_date'])
+                if buy_date<=start_time and start_time<=exp_date:
+                    products.append(p_str)
+            elif 'sold' in product['status'] :
+                sold_date =program.format_time(product['status'].split('/')[1])
+                if buy_date<=start_time and start_time<=sold_date:
+                    products.append(p_str)
+        if len(products)!=0:
+            inventory[start_time]=products
+    
+        start_time=start_time+timedelta(days=1)
+
+    x_axis=inventory.keys()
+    
+    end = len(x_axis)
+    index = 0
+
+    for item in inventory:
+        counter = Counter(inventory[item])
+        for count in counter:
+            if count not in y_coordinates:
+                y_coordinates[count]=[0]*end
+                y_coordinates[count][index]=counter[count]
+            else:
+                y_coordinates[count][index]=counter[count]
+        index+=1
+
+    M=program.format_time(args['date'],'%m-%Y')
+    plt.title('Inventory :'+str(M.month)+'-'+str(M.year))
+    plt.xlabel('dates')
+    plt.ylabel('number in inventory')
+
+    for y in y_coordinates:
+
+        plt.plot(x_axis,y_coordinates[y],label=y)
+
+    plt.legend()
+    plt.show()
+
+"""
+Clears the inventory of the program and returns everything
+back the beginning 
+"""
+
+def clear_inventory():
+    csv_inventory = program.read_csv(file_bought)
+    header=next(csv_inventory)
+    program.write_to_csv(file_bought,header,'w')
+    csv_sales = program.read_csv(file_sold)
+    header = next(csv_sales)
+    program.write_to_csv(file_sold,header,'w')
+
+"""
+    Gets the time that is passed through to 
+    argparse module and returns the datetime object
+"""
+def get_time(args):
+    if args['yesterday']:
+        return program.get_date(program.get_days()-1)
+    elif args['date']:
+        return program.format_time(reverse_date(args['date']))
+    else:
+        return program.get_date(program.get_days())
+
+"""
+    Reverses the date of date to match 
+    iso format
+"""
+def reverse_date(date):
+    arr=date.split('-')
+    arr.reverse()
+    return '-'.join(arr)
+
 """
  Creates a new product id for a product
+ and returns the value of the id
 """
 def product_id(file):
     csv= program.read_csv(file)
     count =0
 
-    for line in csv:
+    for line in csv: 
         count+=1
 
     return count
 
-def get_product(file,pname,pval):
-    products = program.csv_to_dict(file)
+""""
+    Returns a product with the pname and pval 
+"""
+def get_product(key_name,key_val):
+    products=program.csv_to_dict(file_bought)
 
     for product in products:
-        if product[pname]== pval and product['status']=='inventory':
-            return product
+        exp_date = program.format_time(product['expiration_date'])
+        if product[key_name]==key_val and product['status']=='inventory' and exp_date>=program.get_date(program.get_days()):
+                return product
 
     return None
-
-def compare_time(a_time,b_time,c_time):
-    return True if c_time>= a_time and a_time >= b_time else False
-
-"""
-    Returns datetime object from the args that
-    are passed through 
-"""
-def get_time(args):
-    if args['yesterday']:
-        return program.set_days(days-1)
-    elif args['date']:
-        return format_time(program.format_to_iso(args['date']))
-    else:
-        return program.set_days(days)
-
-def format_time(time_str):
-    return datetime.strptime(time_str,'%Y-%m-%d').date()
-
-"""
-    Returns the number of days in the 
-    stored in the setting.txt
-"""
-def return_days():
-    file = open(settings,'r')
-    lines = file.readlines()
-    days= 0
-
-    for line in lines:
-        if 'days' in line:
-            days = line.split('=')
-            return int(days.pop(1))
-
-    return days
-
-"""
-    Changes the number of days in the 
-    setting.txt
-"""
-def change_days(num):
-    file = open(settings,'r')
-    lines = file.readlines()
-    file.close()
-
-    new_lines=[]
-
-    for line in lines:
-        day = ''
-        if 'days' in line:
-            day = line.split('=')
-            day[1]=str(num)
-        
-        new_lines.append('='.join(day))
-
-
-    file= open(settings,'w')
-
-    for line in new_lines:
-        file.write(line)
-
-    return "Time advanced by : "+str(num)+' days'
-    
-"""
-    Returns the current day stored in 
-    the program
-"""
-def get_today():
-    days= return_days()
-    return program.set_days(days)
-
 """
     Parse args and returns them in the form of 
     a dictionary
-"""
+""" 
 def get_arguments():
     parser =argparse.ArgumentParser()
-    parser.add_argument('start',nargs='+',choices='report inventory profit revenue buy sell set')
+    commands='report inventory profit revenue buy sell set advance plot clear'
+    parser.add_argument('start',nargs='+',choices=commands)
+    parser.add_argument('--time',type=int)
     parser.add_argument('--product-name','-p-name')
-    parser.add_argument('--time', type= int)
-    parser.add_argument('--price','--sell-price','--buy-price')
+    parser.add_argument('--price')
     parser.add_argument('--expiration-date','-exp')
     time = parser.add_mutually_exclusive_group()
     time.add_argument('--date')
--- program.py
+++ program.py
@@ -2,6 +2,7 @@ import argparse
 import csv
 from datetime import date
 from datetime import timedelta
+from datetime import datetime
 from rich.console import Console
 from rich.table import Table
 
@@ -9,10 +10,17 @@ from rich.table import Table
 __winc_id__ = 'a2bc36ea784242e4989deb157d527ba0'
 __human_name__ = 'superpy'
 
+settings= 'superpy/settings.txt'
+
 # Your code below this line.
 def main(): 
-    print(format_to_iso('11-11-2021'))
+    print(change_days(2))
  
+"""
+    Returns a list with each line
+    of the csv file as a dictionary with
+    the header as a keys
+""" 
 def csv_to_dict(file):
     csv = read_csv(file)
     header= next(csv)
@@ -22,34 +30,62 @@ def csv_to_dict(file):
         csv_dict = {x:[] for x in header}
         counter = 0
         for key in csv_dict:
-            csv_dict[key]=line[counter]
-            counter+=1
+            if len(line)!=0:
+                csv_dict[key]=line[counter]
+                counter+=1
         products.append(csv_dict)
 
     return products
-
-def format_to_iso(t_str):
-    time_arr= t_str.split('-')
-    time_arr.reverse()
-
-    return '-'.join(time_arr)
 
 def change_row(file,row_id,old_val,new_val):    
     csv = read_csv(file)
     rows=[]
     
     for line in csv:
-        if row_id == line[0] and 'inventory' in line:
-            line[line.index(old_val)] = new_val
+        if row_id == line[0] :
+            line[line.index(old_val)]=new_val
 
         rows.append(line)
 
-    write_to_csv('csv/bought.csv',rows.pop(0),'w')
+    write_to_csv(file,rows.pop(0),'w')
     for row in rows:
-        write_csv('csv/bought.csv',row)
+        write_csv(file,row)
 
-def set_days(num):
+def get_days():
+    file = open(settings,'r')
+    lines = file.readlines()
+
+    for line in lines:
+        if 'days' in line:
+            return int(line.split('=')[1])
+
+    return 0
+
+def advance_time(num):
+    days = get_days()
+    change_days(days+num)
+
+
+def change_days(num):
+    file= open(settings,'w')
+    line ='days='+(str(num))
+    file.write(line)
+
+
+"""
+    Gets the current date and adds num days to it
+    and returns the date
+"""
+def get_date(num):
     return date.today()+timedelta(days=num)
+
+"""
+    Turn time_str,which is a string, into a datetime object
+    and return it. The variable format is the format to which you would
+    want to format the time
+"""
+def format_time(time_str,format='%Y-%m-%d'):
+    return datetime.strptime(time_str,format).date()
 
 def create_dict(arr):
     d_arr ={}
@@ -77,28 +113,22 @@ def delete_row(file_path,product,num):
 
     return read_csv(file_path)
 
-"""
-    Formats an array
-"""
-def get_formatted_string(arr):
-    s =""
-    for x in range(len(arr)):
-        s+="{}      "    
-    return s
-
-#returns a readable csv file from 
-def read_csv(file_path):
-    file =open(file_path,'r')
-    csv_file = csv.reader(file)
-    return csv_file
-
 def create_Table(header,arr,table_title):
     console = Console()
     table = Table(show_header=True, header_style='bold #2070b2',title=table_title)
     for column in header:
         table.add_column(column)
 
+    for item in arr:
+        table.add_row(item['name'],item['price'],str(item['num']),item['exp'])
+
     console.print(table)
+
+#returns a readable csv file from 
+def read_csv(file_path):
+    file =open(file_path,'r')
+    csv_file = csv.reader(file)
+    return csv_file
 
 """
     Append a new row of the data to the file file_path
@@ -107,7 +137,7 @@ def write_csv(file_path,data):
     return write_to_csv(file_path,data,'a')
 
 def write_to_csv(file_path,data,action):
-    file= open(file_path,action)   
+    file= open(file_path,action,newline= '')   
     csv_file= csv.writer(file)
     csv_file.writerow(data)
     return read_csv(file_path)
